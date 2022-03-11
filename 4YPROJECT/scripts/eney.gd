93e72ca1d86324c49d6633a62a2b37f7b3ca1352
emma conlon <emmac2000@outlook.ie>
2022-03-11 16:51:05 +0000

    wandering ai nearly done

--- 4YPROJECT/scripts/eney.gd
+++ 4YPROJECT/scripts/eney.gd
@@ -1,16 +1,85 @@
 extends Spatial
 
-onready var voice = $Level/Navigation/Enemy1
-onready var box1 = $Level/Navigation/walk/box
-onready var box = $Level/Navigation/walk/Position3D
-onready var d = $Level/Navigation/Enemy1
-onready var f = $Level/Navigation/NavigationMeshInstance/GridMap/Player
+onready var pos1 = $Navigation/first_floor_pos/Position3D
+onready var pos2 = $Navigation/first_floor_pos/Position3D2
+onready var pos3 = $Navigation/first_floor_pos/Position3D3
+onready var pos4 = $Navigation/first_floor_pos/Position3D4
+onready var pos5 = $Navigation/first_floor_pos/Position3D5
+onready var pos6 = $Navigation/first_floor_pos/Position3D6
+onready var pos7 = $Navigation/first_floor_pos/Position3D7
+onready var pos8 = $Navigation/first_floor_pos/Position3D8
+onready var pos9 = $Navigation/first_floor_pos/Position3D9
+onready var pos10 = $Navigation/first_floor_pos/Position3D10
+onready var pos11 = $Navigation/first_floor_pos/Position3D11
+onready var pos12 = $Navigation/first_floor_pos/Position3D12
+onready var pos13 = $Navigation/first_floor_pos/Position3D13
+onready var pos14 = $Navigation/first_floor_pos/Position3D14
+onready var pos15 = $Navigation/first_floor_pos/Position3D15
+onready var pos16 = $Navigation/first_floor_pos/Position3D16
+onready var pos17 = $Navigation/first_floor_pos/Position3D17
+onready var pos18 = $Navigation/first_floor_pos/Position3D18
+onready var pos19 = $Navigation/first_floor_pos/Position3D19
+onready var pos20 = $Navigation/first_floor_pos/Position3D20
+onready var pos21 = $Navigation/first_floor_pos/Position3D21
+onready var pos22 = $Navigation/first_floor_pos/Position3D22
+onready var pos23 = $Navigation/first_floor_pos/Position3D23
+onready var pos24 = $Navigation/first_floor_pos/Position3D24
+onready var pos25 = $Navigation/first_floor_pos/Position3D25
+onready var pos26 = $Navigation/first_floor_pos/Position3D26
+onready var pos27 = $Navigation/first_floor_pos/Position3D27
+onready var pos28 = $Navigation/first_floor_pos/Position3D28
+onready var pos29 = $Navigation/first_floor_pos/Position3D29
+onready var pos30 = $Navigation/first_floor_pos/Position3D30
+onready var pos31 = $Navigation/first_floor_pos/Position3D31
+onready var pos32 = $Navigation/first_floor_pos/Position3D32
+onready var pos33 = $Navigation/first_floor_pos/Position3D33
+onready var player = $Navigation/NavigationMeshInstance/GridMap/Player
+var first_floor_pos = []
+
 func _on_Timer_timeout():
-	pass
-	get_tree().call_group("HideandSeek", 'get_target_path', box1.global_transform.origin)
+	get_tree().call_group("first_floor_enemy", 'get_target_path', first_floor_pos)
+	get_tree().call_group("PathFindingAI", 'get_target_path', player.global_transform.origin)
+
 	
+func _ready():
+	first_floor()
+	print(first_floor_pos)
 
 
-func _on_MeshInstance_body_entered(body):
-	if body.name == "Enemy1":
-		d.walking(box1,box)
+func first_floor():
+	first_floor_pos.push_back(pos1.global_transform.origin)
+	first_floor_pos.push_back(pos2.global_transform.origin) 
+	first_floor_pos.push_back(pos3.global_transform.origin)
+	first_floor_pos.push_back(pos4.global_transform.origin)
+	first_floor_pos.push_back(pos5.global_transform.origin)  
+	first_floor_pos.push_back(pos6.global_transform.origin)
+	first_floor_pos.push_back(pos7.global_transform.origin) 
+	first_floor_pos.push_back(pos8.global_transform.origin)
+	first_floor_pos.push_back(pos9.global_transform.origin)
+	first_floor_pos.push_back(pos10.global_transform.origin)  
+	
+	first_floor_pos.push_back(pos11.global_transform.origin)
+	first_floor_pos.push_back(pos12.global_transform.origin) 
+	first_floor_pos.push_back(pos13.global_transform.origin)
+	first_floor_pos.push_back(pos14.global_transform.origin)
+	first_floor_pos.push_back(pos15.global_transform.origin)  
+	first_floor_pos.push_back(pos16.global_transform.origin)
+	first_floor_pos.push_back(pos17.global_transform.origin) 
+	first_floor_pos.push_back(pos18.global_transform.origin)
+	first_floor_pos.push_back(pos19.global_transform.origin)
+	first_floor_pos.push_back(pos20.global_transform.origin)  
+	
+	first_floor_pos.push_back(pos21.global_transform.origin)
+	first_floor_pos.push_back(pos22.global_transform.origin) 
+	first_floor_pos.push_back(pos23.global_transform.origin)
+	first_floor_pos.push_back(pos24.global_transform.origin)
+	first_floor_pos.push_back(pos25.global_transform.origin)  
+	first_floor_pos.push_back(pos26.global_transform.origin)
+	first_floor_pos.push_back(pos27.global_transform.origin) 
+	first_floor_pos.push_back(pos28.global_transform.origin)
+	first_floor_pos.push_back(pos29.global_transform.origin)
+	first_floor_pos.push_back(pos30.global_transform.origin)
+	first_floor_pos.push_back(pos31.global_transform.origin)
+	first_floor_pos.push_back(pos32.global_transform.origin)
+	first_floor_pos.push_back(pos33.global_transform.origin)  
+	
--- 4YPROJECT/scripts/hide and seek/Enemy2.gd
+++ 4YPROJECT/scripts/hide and seek/Enemy2.gd
@@ -7,49 +7,41 @@ var state = STATE.IDLE
 
 onready var start_position = get_global_transform().origin
 onready var target_position = get_global_transform().origin
+var random_pos
 
-var nav = null
+export var speed = 5
+var target=null
 var path = []
+var cur_path_idx = 0
+var velocity = Vector3.ZERO
+var threshold = .5
 
-var stop_distance = 1.0
+onready var nav = get_parent()
+
 
 func _ready():
-	nav = get_parent() # Navigation node is parent of enemy node in the scene tree during run-time
+	randomize()
+	random_pos = randi() % 33
+	print(random_pos)
+	
+func _physics_process(_delta):
+	if path.size() > 0:
+		move_to_target()
 
-func _process(delta):
-	match state:
-		STATE.IDLE:
-			var next_waypoint = Vector3(rand_range(5, 20), translation.y, rand_range(5, 20))
-			target_position = start_position + next_waypoint
-			var p = nav.get_simple_path(start_position, target_position, true)
-			if not p.empty():
-				path = Array(p)
-				path.invert()
-				state = STATE.WANDER
-		STATE.WANDER:
-			var to_walk = delta * move_speed
-			var to_watch = Vector3.UP
-			while to_walk > 0 and path.size() >= 2:
-				var pfrom = path[path.size() - 1]
-				var pto = path[path.size() - 2]
-				to_watch = (pto - pfrom).normalized()
-				var d = pfrom.distance_to(pto)
-				if d <= to_walk:
-					path.remove(path.size() - 1)
-					to_walk -= d
-				else:
-					path[path.size() - 1] = pfrom.linear_interpolate(pto, to_walk / d)
-					to_walk = 0
-				var atpos = path[path.size() - 1]
-				var atdir = to_watch
-				atdir.y = 0
-				var t = Transform()
-				t.origin = atpos
-				t = t.looking_at(atpos + atdir, Vector3.UP)
-				set_transform(t)
-				if path.size() < 2:
-					path = []
-					set_process(false)
-			if (target_position - transform.origin).length() < stop_distance:
-				start_position = get_global_transform().origin
-				state = STATE.IDLE
+
+func move_to_target():	
+	if global_transform.origin.distance_to(path[cur_path_idx]) < threshold:
+		path.remove(0)
+	else:
+		var direction = path[cur_path_idx] - global_transform.origin
+		velocity = direction.normalized() * speed
+
+		move_and_slide(velocity, Vector3.UP)
+
+
+func get_target_path(target_pos):
+	print(target_pos[random_pos])
+	path = nav.get_simple_path(global_transform.origin, target_pos[random_pos])
+	
+
+
